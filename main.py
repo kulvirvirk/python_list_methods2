1906c915e9fdbc6eee6998842c7f3cd14bebfada
kulvirvirk <kvirkdev@gmail.com>
2020-04-08 05:09:37 +0000

    added example to sort the list in reverse order

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 # 1. Create a fruits list
 # 2. using the index() find the index of 'kiwi' from given list
-# 3. use the sort() to sort the given list 
+# 3. use the sort() to sort the given list
 
 
 # 1. Create a fruits list
@@ -15,5 +15,8 @@ print('================************================')
 
 # 3. use the sort() to sort the given list 
 my_fruit_list.sort()
+print(my_fruit_list)
+print('================************================')
+my_fruit_list.sort(reverse=True)
 print(my_fruit_list)
 print('================************================')
