37480e07b2579ece7f3942da75855f38e2b57c6c
kulvirvirk <kvirkdev@gmail.com>
2020-04-08 05:02:23 +0000

    added a example of index() method

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 
 # 1. Create a fruits list
+# 2. using the index() find the index of 'kiwi' from given list
 
+
+# 1. Create a fruits list
 my_fruit_list = ['banana', 'kiwi', 'orange', 'mango', 'watermenlon']
+print(my_fruit_list)
+print('================************================')
+
+# 2. using the index() find the index of 'kiwi' from given list
+print(my_fruit_list.index('kiwi'))
+print('================************================')
