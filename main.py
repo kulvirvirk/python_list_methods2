d5644bf5216ec1dc49eb67c3e98657a1385da3d1
kulvirvirk <kvirkdev@gmail.com>
2020-04-08 05:08:10 +0000

    added the example to sort the list

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 # 1. Create a fruits list
 # 2. using the index() find the index of 'kiwi' from given list
+# 3. use the sort() to sort the given list 
 
 
 # 1. Create a fruits list
@@ -10,4 +11,9 @@ print('================************================')
 
 # 2. using the index() find the index of 'kiwi' from given list
 print(my_fruit_list.index('kiwi'))
+print('================************================')
+
+# 3. use the sort() to sort the given list 
+my_fruit_list.sort()
+print(my_fruit_list)
 print('================************================')
